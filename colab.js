62501345a19887f33558d49dea4399ae3f2a7109
Mike2k24 <166922579+Mike2k24@users.noreply.github.com>
2024-05-04 16:30:34 +0100

    Day 4 changes

--- colab.js
+++ colab.js
@@ -1,9 +1,22 @@
-let num1 = 4
-let num2 = 9
+// let num1 = 4
+// let num2 = 9
 
 
-if (num1 > num2){
-    console.log("FIZZ")
-}else{
-    console.log("BUZZ")
+// if (num1 > num2){
+//     console.log("FIZZ")
+// }else{
+//     console.log("BUZZ")
+// }
+let userNm = document.getElementById("name")
+
+async function randonUser()
+{
+    const response = await fetch("https://randomuser.me/api/")
+    let userRnd = await response.json()
+    console.log(userRnd)
+    alert (`${userRnd.results[0].location.city}`)
 }
+
+
+
+randonUser()
